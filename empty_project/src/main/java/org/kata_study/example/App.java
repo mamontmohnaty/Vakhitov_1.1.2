b5f8fd5ccc350dcb102c683818ddd81f42924bb7
Ravil Vakhitov <mamontmohnaty@gmail.com>
2023-07-17 08:37:40 +0300

    commit empty project

---
+++ empty_project/src/main/java/org/kata_study/example/App.java
@@ -0,0 +1,13 @@
+package org.kata_study.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
